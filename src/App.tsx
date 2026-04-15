5fb73f64d91cf4aecfb838c4268b602187414830
Gabrie Abarca <gabarhdz@outlook.com>
2026-04-15 01:07:41 -0600

    seconf commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import './App.css'
-
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [isDark, setIsDark] = useState(false)
+
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', isDark)
+
+    return () => {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [isDark])
 
   return (
-    <>
-    
-
+    <>  
     </>
   )
 }
