027df461c8fad983f0f0ae88e26e6ac851747fdf
Gabrie Abarca <gabarhdz@outlook.com>
2026-08-11 02:03:29 -0600

    setting sfor vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import path from "path"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
@@ -12,7 +11,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": new URL("./src", import.meta.url).pathname,
     },
   },
 })
